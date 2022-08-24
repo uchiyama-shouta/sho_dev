db7bc8a7b0c5ef38c5286579b7c5b2d36e8c8164
utiyama <ninjin0604@gmail.com>
2022-08-24 20:47:23 +0900

    ディレクトリ構造の変更

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,8 +1,8 @@
 import type { FC, ReactNode } from "react";
 
 import Drawer from "components/Drawer";
-import Footer from "components/Footer";
-import Header from "components/Header";
+import Footer from "components/layout/Footer";
+import Header from "components/layout/Header";
 
 type Props = {
   children: ReactNode;
@@ -10,7 +10,7 @@ type Props = {
 
 const Layout: FC<Props> = ({ children }) => {
   return (
-    <div className="bg-gray-900">
+    <div className="bg-gray-900 text-white">
       <div className="mx-auto flex min-h-screen max-w-7xl flex-col px-4 sm:px-6 lg:px-8">
         <Header />
         <main>{children}</main>
