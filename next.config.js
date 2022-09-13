3c5ed6d9ec946b8a349da1d8fb2cd5f3c14a0337
utiyama <ninjin0604@gmail.com>
2022-09-14 04:04:25 +0900

    i18nの設定の削除

--- next.config.js
+++ next.config.js
@@ -17,7 +17,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 /** @type {import('next').NextConfig} */
 const config = {
   swcMinify: true,
-  i18n: { locales: ["ja"], defaultLocale: "ja" },
   reactStrictMode: true,
   poweredByHeader: false,
   images: {
