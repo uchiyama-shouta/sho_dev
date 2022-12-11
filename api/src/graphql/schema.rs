6f09c8a0a2a2dfcfe10f897b25a53625d54632c9
utiyama <ninjin0604@gmail.com>
2022-12-11 18:53:08 +0900

    format

--- api/src/graphql/schema.rs
+++ api/src/graphql/schema.rs
@@ -14,13 +14,13 @@ pub struct Mutation(PostMutation);
 pub type AppSchema = Schema<Query, Mutation, EmptySubscription>;
 
 pub async fn build_schema() -> AppSchema {
-    let client_options = ClientOptions::parse("mongodb://root:example@localhost:27017")
-        .await
-        .expect("db Connection failed.");
+  let client_options = ClientOptions::parse("mongodb://root:example@localhost:27017")
+    .await
+    .expect("db Connection failed.");
 
-    // Get a handle to the deployment.
-    let client = Client::with_options(client_options).expect("db Connection failed.");
-    Schema::build(Query::default(), Mutation::default(), EmptySubscription)
-        .data(client)
-        .finish()
+  // Get a handle to the deployment.
+  let client = Client::with_options(client_options).expect("db Connection failed.");
+  Schema::build(Query::default(), Mutation::default(), EmptySubscription)
+    .data(client)
+    .finish()
 }
