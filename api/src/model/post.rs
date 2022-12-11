ba672fde7acc20cdcb393c44f8b95c9c04b0d81d
utiyama <ninjin0604@gmail.com>
2022-12-11 18:49:09 +0900

    add is_publish

--- api/src/model/post.rs
+++ api/src/model/post.rs
@@ -4,12 +4,13 @@ use serde::{Deserialize, Serialize};
 
 #[derive(SimpleObject, Clone, Debug, Serialize, Deserialize)]
 pub struct Post {
-    #[serde(rename = "_id")]
-    pub id: String,
-    pub title: String,
-    pub description: String,
-    pub content: String,
-    pub thumbnail: String,
-    pub created_at: DateTime<Utc>,
-    pub updated_at: DateTime<Utc>,
+  #[serde(rename = "_id")]
+  pub id: String,
+  pub title: String,
+  pub description: String,
+  pub content: String,
+  pub thumbnail: String,
+  pub created_at: DateTime<Utc>,
+  pub updated_at: DateTime<Utc>,
+  pub is_publish: bool,
 }
